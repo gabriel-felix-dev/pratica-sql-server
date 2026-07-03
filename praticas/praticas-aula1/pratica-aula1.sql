525df872cacbff2d42c5faca9cadf2dcb6afdd0f
Gabriel <gabrielfelix099@hotmail.com>
2026-07-03 12:51:10 -0300

    feat/material-sql-avancado: Adicionando a resolução do exercício 1 da aula 1

--- praticas/praticas-aula1/pratica-aula1.sql
+++ praticas/praticas-aula1/pratica-aula1.sql
@@ -35,3 +35,97 @@ SELECT  ca.Id as Identificador,
 			ELSE 'Cliente Físico (Regular)'
 		END as Categoria
 	FROM [dbo].[Cliente] AS ca WITH(NOLOCK);
+
+-- Caso A: Controle de Fila e Processamento em Lote (Job de Entrega)
+
+DECLARE @IdPedidoAtual INT;
+
+CREATE TABLE #FilaFaturamento  (
+									IdPedido INT 
+							   );
+
+INSERT INTO #FilaFaturamento (IdPedido)
+	SELECT  Id
+		FROM [dbo].[Pedido] WITH(NOLOCK)
+		WHERE DataEntrega IS NULL
+		ORDER BY DataPromessa ASC;
+
+WHILE EXISTS  (
+			     SELECT TOP 1 1
+					FROM #FilaFaturamento
+              )
+	BEGIN
+	
+		SELECT  TOP 1 @IdPedidoAtual = IdPedido
+			FROM #FilaFaturamento;
+
+	PRINT 'Processando baixa de faturamento e estoque do Pedido ID: ' + CAST(@IdPedidoAtual AS VARCHAR(10));
+
+	DELETE TOP (1)
+		FROM #FilaFaturamento;
+
+		SET @IdPedidoAtual = NULL;
+
+	END
+
+DROP TABLE #FilaFaturamento;
+
+-- Exercício 1
+
+DECLARE @IdPedido INT;
+DECLARE @DataPromessaPedido DATE;
+DECLARE @DataEntregaPedido DATE;
+
+SET @IdPedido = 1;
+
+CREATE TABLE #Pedido (
+					    IdPedido INT,
+						IdCliente INT,
+						DataPedido DATE,
+						DataPromessa DATE,
+						DataEntrega DATE
+					 )
+INSERT INTO #Pedido (IdPedido, IdCliente, DataPedido, DataPromessa, DataEntrega)
+ SELECT  TOP 1 *
+	FROM [dbo].[Pedido] AS pe WITH(NOLOCK)
+	WHERE pe.Id = @IdPedido;
+
+IF NOT EXISTS (
+				 SELECT  TOP 1 1
+					 FROM #Pedido
+			  )
+	BEGIN
+		PRINT 'Erro: Pedido de ID ' + CAST(@IdPedido AS VARCHAR(4)) + ' não encontrado no sistema.';
+	END
+
+SELECT @DataEntregaPedido = pe.DataEntrega,
+	   @DataPromessaPedido = pe.DataPromessa
+	FROM #Pedido AS pe;
+
+IF EXISTS (
+			 SELECT  TOP 1 1
+			 	 FROM #Pedido
+			 	 WHERE DataEntrega IS NULL  
+		   )
+	BEGIN
+		PRINT 'Pedido ' + CAST(@IdPedido AS VARCHAR(4)) + ' pendente de entrega. Prazo prometido: ' + CAST(@DataPromessaPedido AS VARCHAR);
+	END
+
+--UPDATE #Pedido
+--	SET DataEntrega = '2026-07-03'
+--	WHERE IdPedido = @IdPedido;
+
+--SELECT @DataEntregaPedido = pe.DataEntrega,
+--	   @DataPromessaPedido = pe.DataPromessa
+--	FROM #Pedido AS pe;
+
+IF EXISTS (
+             SELECT   TOP 1 1
+				FROM #Pedido
+				WHERE DataEntrega IS NOT NULL
+		  )
+	BEGIN
+		PRINT 'Pedido ' + CAST(@IdPedido AS VARCHAR) + ' entregue com sucesso em: ' + CAST(@DataEntregaPedido AS VARCHAR);
+	END
+
+DROP TABLE #Pedido;
