93ba92834e0ebfa9a87de416eaa4fb70c2d47517
Gabriel <gabrielfelix099@hotmail.com>
2026-07-06 12:32:36 -0300

    feat/material-sql-avancado: Adicionando a resolução do exercicio 1 e o arquivo para prática da aula 2

--- praticas/praticas-aula1/pratica-aula1.sql
+++ praticas/praticas-aula1/pratica-aula1.sql
@@ -1,5 +1,7 @@
 USE WoodCraft;
 
+-- Declaração de variáveis
+
 DECLARE @IdCliente INT;
 DECLARE @NomeCliente VARCHAR(100);
 
@@ -11,6 +13,8 @@ SELECT  @NomeCliente = Nome
 
 PRINT @NomeCliente;
 
+-- IF / ELSE
+
 IF @IdCliente IS NOT NULL
 	BEGIN 
 		PRINT 'O cliente foi informado.';
@@ -20,6 +24,8 @@ ELSE
 		PRINT 'O cliente não foi informado.';
 	END
 
+-- WHILE
+
 DECLARE @Contador INT = 1;
 
 WHILE @Contador <= 5
@@ -27,6 +33,8 @@ WHILE @Contador <= 5
 		PRINT 'Iteração: ' + CAST(@Contador AS VARCHAR(2));
 		SET @Contador = @Contador + 1;
 	END
+
+-- CASE WHEN
 
 SELECT  ca.Id as Identificador,
 		ca.Nome as Nome,
@@ -129,3 +137,78 @@ IF EXISTS (
 	END
 
 DROP TABLE #Pedido;
+
+-- Teste
+
+-- Uma tabela temporária será criada com uma informação dentro. Um loop será rodado. Enquanto esse loop for menor que 5, a tabela temporária não poderá ser excluída.
+
+DECLARE @Iterador INT = 0;
+
+CREATE TABLE #TabelaTemporaria(
+							     IdPedido INT
+                              );
+
+INSERT INTO #TabelaTemporaria (IdPedido)
+	SELECT  pe.Id
+		FROM Pedido AS pe WITH(NOLOCK);
+
+WHILE @Iterador < 5
+	BEGIN 
+		PRINT 'Valor do contador: ' + CAST(@Iterador AS VARCHAR)
+		SET @Iterador = @Iterador + 1;
+	END;
+
+IF @Iterador = 5
+	BEGIN
+		DROP TABLE #TabelaTemporaria;
+		PRINT 'Tabela deletada'
+	END;
+
+-- Exercício 2
+
+SELECT  *
+	FROM Pedido AS pe WITH(NOLOCK)
+
+SELECT  *
+	FROM Cliente AS cl WITH(NOLOCK)
+
+DECLARE @IdPedido2 INT; 
+DECLARE @NomeCliente2 VARCHAR(100);
+DECLARE @DiasAtraso INT;
+DECLARE @ContadorLoop INT = 1;
+DECLARE @TotalPedidos INT;
+
+DECLARE @PedidosAtrasados TABLE (
+								   IdPedido INT,
+								   NomeCliente VARCHAR(100),
+								   DiasAtraso INT
+							    );
+
+INSERT INTO @PedidosAtrasados (IdPedido, NomeCliente, DiasAtraso)
+	SELECT  pe.Id as IdentificadorPedido,
+			cl.Nome as Cliente,
+			DATEDIFF(DAY, pe.DataPromessa, GETDATE()) as DiasEmAtraso
+		FROM Pedido AS pe WITH(NOLOCK)
+			INNER JOIN Cliente AS cl WITH(NOLOCK)
+				ON cl.Id = pe.IdCliente
+		WHERE pe.DataEntrega IS NULL
+			AND pe.DataPromessa < GETDATE();
+
+SELECT  @TotalPedidos = COUNT(*) 
+	FROM @PedidosAtrasados;
+	
+WHILE @ContadorLoop <= @TotalPedidos
+	BEGIN
+		SELECT  TOP 1 @IdPedido2 = IdPedido,
+				@NomeCliente2 = NomeCliente,
+				@DiasAtraso = DiasAtraso
+		FROM @PedidosAtrasados
+		WHERE IdPedido = @ContadorLoop;
+
+		IF @IdPedido2 IS NOT NULL
+			BEGIN 
+				PRINT'O Pedido ID ' + CAST(@IdPedido2 AS VARCHAR) + ' do cliente ' + CAST(@NomeCliente2 AS VARCHAR) + ' está atrasado em ' + CAST(@DiasAtraso AS VARCHAR) + ' dias.';
+			END
+
+		SET @ContadorLoop = @ContadorLoop + 1;
+	END
---
+++ praticas/praticas-aula2/pratica-aula2.sql
@@ -0,0 +1,5 @@
+USE WoodCraft;
+
+-- CTEs
+
+
