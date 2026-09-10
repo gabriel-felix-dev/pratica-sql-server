1ef597c64cc45905535b554b0a58f894ce33c80c
Gabriel <gabrielfelix099@hotmail.com>
2026-09-10 13:27:43 -0300

    feat/aula-ricardo: Adicionadno alterações na PROC de validação de limite

--- praticas/pratica-ricbank/SP/USP_RevisarLimites_GabrielFelix.sql
+++ praticas/pratica-ricbank/SP/USP_RevisarLimites_GabrielFelix.sql
@@ -17,7 +17,7 @@ CREATE PROCEDURE [dbo].[USP_RevisarLimites_GabrielFelix]
 	/*
 		Documentacao
 		Arquivo Fonte............: USP_RevisarLimites_GabrielFelix.sql
-		Objetivo.................: 
+		Objetivo.................: Revisar o limite das contas
 		Auto.....................: Gabriel Felix
 		Data.....................: 09/09/2026
 		Ex.......................: DBCC FREEPROCCACHE
@@ -33,7 +33,8 @@ CREATE PROCEDURE [dbo].[USP_RevisarLimites_GabrielFelix]
 										   DATEDIFF(MILLISECOND, @DataInicio,  GETDATE()) As TempoExecucao
 
 		Retornos.................: 0 - Sucesso
-								   1 - Erro - TipoLimite não encontrado
+								   -1 - Erro - TipoLimite não encontrado
+								   -2 - Erro - 	
 	*/
 	BEGIN
 		-- Valida se a data é nula para definir a data de hoje
@@ -41,9 +42,27 @@ CREATE PROCEDURE [dbo].[USP_RevisarLimites_GabrielFelix]
 			SET @DataReferencia = CAST(GETDATE() AS DATE)
 
 		-- Valida se o TipoLimite existe
-		IF NOT EXISTS ()
+		IF NOT EXISTS (
+		                 SELECT  TOP 1 1
+							 FROM [dbo].[TipoLimite]
+							 WHERE Id = @IdTipoLimite
+					  )
 			RETURN -1 
-		
+	    
+		-- Pegar ano e mês da data informada pelo usuário
+		DECLARE @Mes TINYINT = MONTH(@DataReferencia),
+				@Ano SMALLINT = YEAR(@DataReferencia);
+
+		-- Definir variáveis para armazenar os três meses anteriores
+
+		DECLARE @PrimeiroMes DATE = DATEADD(MONTH, -1, DATEFROMPARTS(@Ano, @Mes, 1))--,		
+		DECLARE @SegundoMes DATE =  DATEADD(MONTH, -1, @PrimeiroMes);
+		DECLARE @TerceiroMes DATE =  DATEADD(MONTH, -1, @SegundoMes);
+		--		@SegundoMes DATE = DATEADD(MONTH, -2, DATEFROMPARTS(@Ano, @Mes, 1)),
+		--		@TerceiroMes DATE = DATEADD(MONTH, -3, DATEFROMPARTS(@Ano, @Mes, 1));
+
+
+
 
 		RETURN 0
 	END
